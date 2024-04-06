2e0960ef336ec4aedf165e3f7db27b238089724c
Anna Manrodt <157744313+AnnaManrodt@users.noreply.github.com>
2024-04-06 13:58:58 -0500

    fixed styling broke toggle on blog page

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,10 +1,14 @@
-
+//const back = document.querySelector(".button")
 const meow = localStorage.getItem("identity");
 console.log(meow)
 
-function myFunction() {
+function toggle() {
   var element = document.body;
-  element.classList.toggle("dark")
+  element.classList.toggle("dark");
   //sun.append("&#9790;");
+  }
 
-  ;}
+
+document.getElementById("backButton").addEventListener("click", function() {
+window.history.go(-1);
+});
