a5b442763bea8be4e72d686feb8ae8555923a33e
Anna Manrodt <157744313+AnnaManrodt@users.noreply.github.com>
2024-04-07 10:27:39 -0500

    added array

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,16 +1,44 @@
 const back = document.querySelector(".button")
-const meow = localStorage.getItem("identity");
-console.log(meow)
-
+const string = localStorage.getItem("formInfo")
+let formInfo = JSON.parse(string) || [];
+let data1 = document.querySelector(".data1")
 function toggle() {
-  var element = document.body;
-  element.classList.toggle("dark");
-  //sun.append("&#9790;");
-  }
+    var element = document.body;
+    element.classList.toggle("dark");
+    //sun.append("&#9790;");
+    }
 
+    //this makes the back button work
 
-  back.addEventListener('click', function(event){
-    event.preventDefault()
-  window.location.href = "index.html";
-   // doesnt work yet
-  })
+    back.addEventListener('click', function(event){
+      event.preventDefault()
+    window.location.href = "index.html";
+    
+    })
+
+  for (let i = 0; i < formInfo.length; i++){
+    const section = document.createElement("section")
+    section.classList.add();
+
+    let html = `
+    <h3>${formInfo.username}</h3>
+    <p>${formInfo.title}</p>
+    <p>${formInfo.content}</p>`
+
+    section.innerHTML = html;
+  data1.appendChild(section);
+
+}
+
+//   if (formInfo){
+//     document.querySelector("#username").textContent = formInfo.username;
+//     document.querySelector("#title").textContent = formInfo.title;
+//     document.querySelector("#username").textContent = formInfo.content;
+//   }
+
+//   let gotInfo= [
+// formInfo.username,
+// formInfo.title,
+// formInfo.content
+// ]
+ 
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,46 +1,30 @@
-//reading and writing from local storage anything that both pages neeeds 
-// something for the graphics?
-//const username = document.querySelector("#Username");
-//let username;
-//let title;
-//let container;
-// let mode = "dark";
-// const container = document.querySelector('.container');
-
 const sun = document.querySelector(".sun");
+const string = localStorage.getItem("formInfo");
+let blog = JSON.parse(string) || [];
 
 function myFunction() {
   var element = document.body;
   element.classList.toggle("dark")
   //sun.append("&#9790;");
   ;}
-
-let formInfo = {
-username : document.querySelector("#username"),
-title : document.querySelector("#title"),
-content : document.querySelector("#content"),
-}
-//console.log(formInfo)
-
-let username = document.querySelector("#username");
-let title = document.querySelector("#title");
-let content = document.querySelector("#content");
-
-console.log(username)
-
-//formInfo.push({username, title, content})
-
-const submit = document.querySelector("#submitButton");
+//this makes the light dark toggle work
 
 
+let submit = document.querySelector("#submitButton");
 
-let identity = JSON.stringify(localStorage.getItem("formInfo.value"))
 
-console.log(identity)
+// let identity = JSON.stringify(localStorage.getItem("formInfo.value"))
+
 
 submit.addEventListener('click', function(event){
   event.preventDefault()
-localStorage.setItem('formInfo', identity);
+  let  something = {
+    username : document.querySelector("input[id= 'username']").value,
+    title : document.querySelector("input[id= 'title']").value,
+    content : document.querySelector("input[id= 'content']").value
+    };
+    blog.push(something);
+localStorage.setItem('formInfo', JSON.stringify(blog));
 window.location.href = "blog.html";
  // doesnt work yet
 })
