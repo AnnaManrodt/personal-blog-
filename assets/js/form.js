13096378a0e0cbd6a1da2a904ff09d45d1f558b7
Anna Manrodt <157744313+AnnaManrodt@users.noreply.github.com>
2024-04-10 10:55:34 -0500

    better use of files

--- assets/js/form.js
+++ assets/js/form.js
@@ -5,3 +5,23 @@
 // }
 // else{};
 //
+
+submit.addEventListener('click', function(event){
+  if (form.checkValidity() === false){
+    console.log("form invalid");
+    form.querySelectorAll("input").forEach(i=>{
+      i.classList.add("invalid");
+    });
+  }
+  else {
+  event.preventDefault()
+  let  something = {
+    username : document.querySelector("input[id= 'username']").value,
+    title : document.querySelector("input[id= 'title']").value,
+    content : document.querySelector("input[id= 'content']").value
+    };
+    blog.push(something);
+localStorage.setItem('formInfo', JSON.stringify(blog));
+window.location.href = "blog.html"; }
+// doesnt work yet
+}) 
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -17,33 +17,6 @@ let submit = document.querySelector("#submitButton");
 // let identity = JSON.stringify(localStorage.getItem("formInfo.value"))
 
 
-submit.addEventListener('click', function(event){
-      if (form.checkValidity() === false){
-        console.log("form invalid");
-        form.querySelectorAll("input").forEach(i=>{
-          i.classList.add("invalid");
-        });
-      }
-      else {
-      event.preventDefault()
-      let  something = {
-        username : document.querySelector("input[id= 'username']").value,
-        title : document.querySelector("input[id= 'title']").value,
-        content : document.querySelector("input[id= 'content']").value
-        };
-        blog.push(something);
-    localStorage.setItem('formInfo', JSON.stringify(blog));
-    window.location.href = "blog.html"; }
-    // doesnt work yet
-}) 
-
-
-
-
-
-
-
-
 // function submitForm (action) {
 //   var form = document.getElementById('form')
 //   form.action = action
